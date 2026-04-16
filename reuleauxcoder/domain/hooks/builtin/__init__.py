6b5998f3baedb95536a7a40085dcee821c37c60a
RC-CHN <badbatch0x01@gmail.com>
2026-04-17 00:53:03 +0800

    fix: export ProjectContextStartupNotifier from builtin  hooks __init__

--- reuleauxcoder/domain/hooks/builtin/__init__.py
+++ reuleauxcoder/domain/hooks/builtin/__init__.py
@@ -2,6 +2,14 @@
 
 from reuleauxcoder.domain.hooks.builtin.tool_output import ToolOutputTruncationHook
 from reuleauxcoder.domain.hooks.builtin.tool_policy import ToolPolicyGuardHook
-from reuleauxcoder.domain.hooks.builtin.project_context import ProjectContextHook
+from reuleauxcoder.domain.hooks.builtin.project_context import (
+    ProjectContextHook,
+    ProjectContextStartupNotifier,
+)
 
-__all__ = ["ToolOutputTruncationHook", "ToolPolicyGuardHook", "ProjectContextHook"]
+__all__ = [
+    "ToolOutputTruncationHook",
+    "ToolPolicyGuardHook",
+    "ProjectContextHook",
+    "ProjectContextStartupNotifier",
+]
